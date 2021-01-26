444080b8665dab45c059e658660faf3a11f98c84
LucasArtel33 <lucas.artel@lapiscine.pro>
2021-01-26 15:10:21 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import { Provider } from "react-redux";
+
+import store from "./src/redux/store";
+
+import RecipesListScreen from "./src/components/RecipesEpic/RecipesListScreen";
+import RecipeDetailsScreen from "./src/components/RecipesEpic/RecipeDetailsScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <RecipesListScreen />
+        <RecipeDetailsScreen />
+      </View>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/components/RecipesEpic/RecipeDetailsScreen/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const RecipeDetailsScreen = () => {
+  return (
+    <View>
+      <Text>RecipeDetailsScreen</Text>
+    </View>
+  );
+};
+
+export default RecipeDetailsScreen;
