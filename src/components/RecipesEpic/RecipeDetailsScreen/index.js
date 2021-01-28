58a79948ad06f3ebe768e140062a56a3e29d4839
LucasArtel33 <lucas.artel@lapiscine.pro>
2021-01-28 09:47:17 +0100

    get recipes requete

--- src/components/RecipesEpic/RecipeDetailsScreen/index.js
+++ src/components/RecipesEpic/RecipeDetailsScreen/index.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
-const RecipeDetailsScreen = () => {
+const RecipeDetailsScreen = ({ navigation }) => {
   return (
     <View>
       <Text>RecipeDetailsScreen</Text>
+      <Button
+        title="Back"
+        onPress={() => {
+          navigation.goBack();
+        }}
+      />
     </View>
   );
 };
--- src/components/RecipesEpic/RecipesListScreen/index.js
+++ src/components/RecipesEpic/RecipesListScreen/index.js
@@ -1,12 +1,41 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 
-const RecipesListScreen = () => {
+import { fetchRecipes } from "../../../api/recipes";
+import { getRecipes } from "../../../redux/selectors";
+
+const RecipesListScreen = ({ navigation }) => {
+  const dispatch = useDispatch();
+  const recipes = useSelector(getRecipes);
+
+  useEffect(() => {
+    fetchRecipes(dispatch);
+  }, []);
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>RecipesListScreen</Text>
+      {recipes.map((recipe) => {
+        return <Text>{recipe.title}</Text>;
+      })}
+      <Button
+        onPress={() => {
+          navigation.navigate("RecipeDetails");
+        }}
+        title="Go to Detail"
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default RecipesListScreen;
